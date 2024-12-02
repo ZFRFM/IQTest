9d56f67cbfa969df59d4fdf7c01c5768d7f81258
ZFRFM <faimi14102001@gmail.com>
2024-12-02 12:15:25 +0400

    Implemented VacancySearchBar

--- features/vacancy/src/main/java/ru/faimizufarov/vacancy/components/VacancyTopAppBar.kt
+++ features/vacancy/src/main/java/ru/faimizufarov/vacancy/components/VacancyTopAppBar.kt
@@ -1,13 +1,9 @@
 package ru.faimizufarov.vacancy.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
@@ -17,16 +13,18 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ru.faimizufarov.core.R
+import ru.faimizufarov.core.theme.WorkerTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -77,6 +75,21 @@ fun VacancyTopAppBar() {
                     color = Color.Black
                 )
             }
-        }
+        },
+        colors = TopAppBarColors(
+            containerColor = Color(0xFFF4F4F4),
+            scrolledContainerColor = Color.Transparent,
+            navigationIconContentColor = Color(0xFF444748),
+            titleContentColor = Color.Transparent,
+            actionIconContentColor = Color.Transparent
+        )
     )
+}
+
+@Preview
+@Composable
+fun PreviewVacancyTopAppBar() {
+    WorkerTheme {
+        VacancyTopAppBar()
+    }
 }
--- features/vacancy/src/main/java/ru/faimizufarov/vacancy/screens/VacancyScreen.kt
+++ features/vacancy/src/main/java/ru/faimizufarov/vacancy/screens/VacancyScreen.kt
@@ -1,40 +1,17 @@
 package ru.faimizufarov.vacancy.screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import ru.faimizufarov.core.R
 import ru.faimizufarov.core.theme.WorkerTheme
-import ru.faimizufarov.vacancy.components.VacancyFloatingActionButton
-import ru.faimizufarov.vacancy.components.VacancyTopAppBar
+import ru.faimizufarov.vacancy.components.VacancySearchBar
 
 @Composable
 fun VacancyScreen() {
@@ -42,9 +19,10 @@ fun VacancyScreen() {
         modifier = Modifier
             .fillMaxSize()
             .background(Color(0xFFF4F4F4)),
-        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        VacancySearchBar()
+
         Text(
             text = "In progress",
             textAlign = TextAlign.Center,
